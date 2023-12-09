6664a2975021d335895d39be1bd57df59b92aac7
mirza-bilal-amjad <69304697+mirza-bilal-amjad@users.noreply.github.com>
2023-12-09 15:40:58 +0500

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 4,
+  semi: true,
+};
